7cbf73c34d5c8292e1cd01d429e23866700dbe28
jehd <jehd.jehzer@gmail.com>
2024-07-01 07:12:11 +0800

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -11,6 +12,8 @@ import userRoutes from "./routes/user.routes.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
@@ -20,9 +23,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!!");
-// });
+// codes for deployment start
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+// codes for deployment end
 
 server.listen(PORT, () => {
   connectToMongoDB();
